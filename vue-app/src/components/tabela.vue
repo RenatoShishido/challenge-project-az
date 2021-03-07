d8924b4eeaddf190fe435e2ae2d7edbd01291dab
Renato <renato.re2012@hotmail.com>
2021-03-07 13:54:34 -0400

    Views e Components
    Modificando o layout da tela do Administrado para ele ficar com campos expansivos e implementando novas funcionalidades no componente tabela.

--- vue-app/src/components/tabela.vue
+++ vue-app/src/components/tabela.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-container fluid>
+  <v-container>
     <v-data-iterator
       :items="items"
       :items-per-page.sync="itemsPerPage"
@@ -24,53 +24,52 @@
           <v-col cols="12">
             <thead>
               <tr>
-                <th cols="2" class="text-left px-2 title font-weight-bold">
+                <th class="text-left px-2 subtitle font-weight-bold">
                   Nome
                 </th>
-                <th cols="3" class="text-center px-2 title font-weight-bold">
+                <th class="text-center px-2 subtitle font-weight-bold">
                   Email
                 </th>
-                <th cols="1" class="text-left px-2 title font-weight-bold">
+                <th class="text-left px-2 subtitle font-weight-bold">
                   Utensilio
                 </th>
-                <th cols="5" class="text-left px-2 title font-weight-bold">
+                <th class="text-left px-2 subtitle font-weight-bold">
                   Descricao
                 </th>
-                <th cols="5" class="text-left px-2 title font-weight-bold">
+                <th class="text-left px-2 subtitle font-weight-bold">
                   Alterar
                 </th>
-                <th cols="5" class="text-left px-2 title font-weight-bold">
+                <th class="text-left px-2 subtitle font-weight-bold">
                   Deletar
                 </th>
               </tr>
             </thead>
             <tbody>
               <tr v-for="item in items" :key="item.name">
-                <td cols="2" class="px-2">{{ item.name }}</td>
-                <td cols="3" class="px-2">{{ item.email }}</td>
-                <td cols="1" class="px-2">{{ item.utensilio }}</td>
-                <td cols="5" class="px-2">{{ item.descricao }}</td>
-                <td cols="5" class="px-2">
+                <td class="px-2">{{ item.name }}</td>
+                <td class="px-2">{{ item.email }}</td>
+                <td class="px-2">{{ item.utensilio }}</td>
+                <td class="px-2">{{ item.descricao }}</td>
+                <td class="px-2">
                   <Button
                     @defaultAction="cleanFields"
                     :typeButton="{
                       color: 'green',
                       fab: 'fab',
                       icon: 'mdi-pencil',
-                      outlined: 'outlined'
+                      outlined: 'outlined',
                     }"
                   />
                 </td>
                 <td cols="5" class="px-2">
                   <Dialog
-                  :typeButton="{
+                    :typeButton="{
                       color: 'red',
                       fab: 'fab',
                       icon: 'mdi-delete',
-                      outlined: 'outlined'
+                      outlined: 'outlined',
                     }"
                   />
-
                 </td>
               </tr>
             </tbody>
@@ -80,21 +79,7 @@
 
       <template v-slot:footer>
         <v-row class="mt-2" align="center" justify="center">
-          <span class="grey--text">Items per page</span>
           <v-menu offset-y>
-            <template v-slot:activator="{ on, attrs }">
-              <v-btn
-                dark
-                text
-                color="primary"
-                class="ml-2"
-                v-bind="attrs"
-                v-on="on"
-              >
-                {{ itemsPerPage }}
-                <v-icon>mdi-chevron-down</v-icon>
-              </v-btn>
-            </template>
             <v-list>
               <v-list-item
                 v-for="(number, index) in itemsPerPageArray"
@@ -137,16 +122,13 @@ import Dialog from "./dialog";
 export default {
   components: {
     Button,
-    Dialog
+    Dialog,
   },
   data() {
     return {
-      itemsPerPageArray: [4, 8, 12],
       search: "",
       filter: {},
-      sortDesc: false,
       page: 1,
-      itemsPerPage: 4,
       sortBy: "name",
       items: [
         {
@@ -167,63 +149,8 @@ export default {
           utensilio: "Panela de Pressao",
           descricao: "Panela com pouco uso bem conservada pronta para usar",
         },
-        {
-          name: "Renato Tomio",
-          email: "renato.re2012@hotmail.com",
-          utensilio: "Panela de Pressao",
-          descricao: "Panela com pouco uso bem conservada pronta para usar",
-        },
-        {
-          name: "Renato Tomio",
-          email: "renato.re2012@hotmail.com",
-          utensilio: "Panela de Pressao",
-          descricao: "Panela com pouco uso bem conservada pronta para usar",
-        },
-        {
-          name: "Renato Tomio",
-          email: "renato.re2012@hotmail.com",
-          utensilio: "Panela de Pressao",
-          descricao: "Panela com pouco uso bem conservada pronta para usar",
-        },
-        {
-          name: "Renato Tomio",
-          email: "renato.re2012@hotmail.com",
-          utensilio: "Panela de Pressao",
-          descricao: "Panela com pouco uso bem conservada pronta para usar",
-        },
-        {
-          name: "Renato Tomio",
-          email: "renato.re2012@hotmail.com",
-          utensilio: "Panela de Pressao",
-          descricao: "Panela com pouco uso bem conservada pronta para usar",
-        },
-        {
-          name: "Renato Tomio",
-          email: "renato.re2012@hotmail.com",
-          utensilio: "Panela de Pressao",
-          descricao: "Panela com pouco uso bem conservada pronta para usar",
-        },
       ],
     };
-  },
-  computed: {
-    numberOfPages() {
-      return Math.ceil(this.items.length / this.itemsPerPage);
-    },
-    filteredKeys() {
-      return this.keys.filter((key) => key !== "Name");
-    },
-  },
-  methods: {
-    nextPage() {
-      if (this.page + 1 <= this.numberOfPages) this.page += 1;
-    },
-    formerPage() {
-      if (this.page - 1 >= 1) this.page -= 1;
-    },
-    updateItemsPerPage(number) {
-      this.itemsPerPage = number;
-    },
   },
 };
 </script>
--- vue-app/src/views/Administrador.vue
+++ vue-app/src/views/Administrador.vue
@@ -2,96 +2,128 @@
   <v-main>
     <v-container fill-height>
       <v-row>
-        <v-col cols="12" md="6" xl="6" lg="6">
-          <v-row align="center">
-            <v-col cols="12">
-              <h1 class="d-flex justify-start my-1 display-1">
-                Cadastro do Usuario
-              </h1>
-            </v-col>
-            <v-col cols="12" md="6">
-              <v-text-field
-                v-model="fields.name"
-                :rules="defaultRules"
-                outlined
-                label="Name"
-                required
-              ></v-text-field>
-            </v-col>
-            <v-col cols="12" md="6">
-              <v-text-field
-                v-model="fields.telefone"
-                :rules="telefoneRules"
-                outlined
-                label="Telefone"
-                required
-              ></v-text-field>
-            </v-col>
-            <v-col cols="12">
-              <v-text-field
-                v-model="fields.email"
-                :rules="emailRules"
-                outlined
-                label="E-mail"
-                required
-              ></v-text-field>
-            </v-col>
-            <v-col cols="12">
-              <h1 class="d-flex justify-start mb-2 display-1">
-                Cadastro do Utensilio
-              </h1>
-            </v-col>
-            <v-col cols="12">
-              <v-text-field
-                v-model="fields.nomeUtensilio"
-                outlined
-                :rules="defaultRules"
-                label="Nome Utensilio"
-                required
-              ></v-text-field>
-            </v-col>
-            <v-col cols="12">
-              <v-textarea
-                v-model="fields.descricao"
-                outlined
-                :rules="defaultRules"
-                label="Descricao"
-                required
-              ></v-textarea>
-            </v-col>
-            <v-col cols="12" class="d-flex justify-center">
-              <Button
-                @defaultAction="cadastrarDoacao"
-                :typeButton="{
-                  color: 'success',
-                  text: 'CADASTRAR',
-                  class: 'mr-4 white--text',
-                  outlined: 'outlined',
-                }"
-              />
-              <Button
-                @defaultAction="cleanFields"
-                :typeButton="{
-                  color: 'red',
-                  text: 'LIMPAR',
-                  class: 'mr-4 white--text',
-                  outlined: 'outlined',
-                }"
-              />
-            </v-col>
-          </v-row>
-        </v-col>
-        <v-col cols="12" md="6" xl="6" lg="6">
-          <v-row>
-            <v-col cols="12">
-              <h1 class="d-flex justify-start mb-2 display-1">
+        <v-col>
+          <v-expansion-panels focusable inset hover v-model="panel">
+            <v-expansion-panel>
+              <v-expansion-panel-header class="title">
+                Cadastro de Pessoa
+              </v-expansion-panel-header>
+              <v-expansion-panel-content>
+                <v-row align="center">
+                  <v-col cols="12">
+                    <h1 class="d-flex justify-start my-1 display-1">
+                      Cadastro do Usuario
+                    </h1>
+                  </v-col>
+                  <v-col cols="12" md="6">
+                    <v-text-field
+                      v-model="fields.name"
+                      :rules="defaultRules"
+                      outlined
+                      label="Name"
+                      required
+                    ></v-text-field>
+                  </v-col>
+                  <v-col cols="12" md="6">
+                    <v-text-field
+                      v-model="fields.telefone"
+                      :rules="telefoneRules"
+                      outlined
+                      label="Telefone"
+                      required
+                    ></v-text-field>
+                  </v-col>
+                  <v-col cols="12">
+                    <v-text-field
+                      v-model="fields.email"
+                      :rules="emailRules"
+                      outlined
+                      label="E-mail"
+                      required
+                    ></v-text-field>
+                    <Button
+                      @defaultAction="push"
+                      :typeButton="{
+                        color: 'green',
+                        text: 'Continuar',
+                        class: 'mr-4 white--text',
+                        outlined: 'outlined',
+                      }"
+                    />
+                  </v-col>
+                </v-row>
+              </v-expansion-panel-content>
+            </v-expansion-panel>
+            <v-expansion-panel>
+              <v-expansion-panel-header class="title">
+                Cadastro de Utensilio
+              </v-expansion-panel-header>
+              <v-expansion-panel-content>
+                <v-row>
+                  <v-col cols="12">
+                    <h1 class="d-flex justify-start mb-2 display-1">
+                      Cadastro do Utensilio
+                    </h1>
+                  </v-col>
+                  <v-col cols="12">
+                    <v-text-field
+                      v-model="fields.nomeUtensilio"
+                      outlined
+                      :rules="defaultRules"
+                      label="Nome Utensilio"
+                      required
+                    ></v-text-field>
+                  </v-col>
+                  <v-col cols="12">
+                    <v-textarea
+                      v-model="fields.descricao"
+                      outlined
+                      :rules="defaultRules"
+                      label="Descricao"
+                      required
+                    ></v-textarea>
+                  </v-col>
+                  <v-col cols="12" class="d-flex justify-center">
+                    <Button
+                      @defaultAction="cadastrarDoacao"
+                      :typeButton="{
+                        color: 'success',
+                        text: 'CADASTRAR',
+                        class: 'mr-4 white--text',
+                        outlined: 'outlined',
+                      }"
+                    />
+                    <Button
+                      @defaultAction="cleanFields"
+                      :typeButton="{
+                        color: 'red',
+                        text: 'LIMPAR',
+                        class: 'mr-4 white--text',
+                        outlined: 'outlined',
+                      }"
+                    />
+                  </v-col>
+                </v-row>
+              </v-expansion-panel-content>
+            </v-expansion-panel>
+            <v-expansion-panel>
+              <v-expansion-panel-header class="title">
                 Listagem de Usuarios
-              </h1>
-            </v-col>
-            <v-col cols="12">
-              <Table />
-            </v-col>
-          </v-row>
+              </v-expansion-panel-header>
+              <v-expansion-panel-content>
+                <v-row>
+                  <v-col cols="12">
+                    <h1 class="d-flex justify-start mb-2 display-1">
+                      Listagem de Usuarios
+                    </h1>
+                  </v-col>
+                  <v-col cols="12">
+                    <Table />
+                  </v-col>
+                </v-row>
+              </v-expansion-panel-content>
+            </v-expansion-panel>
+          </v-expansion-panels>
         </v-col>
       </v-row>
     </v-container>
@@ -109,6 +141,7 @@ export default {
   },
   data: () => ({
     fields: {},
+    panel: [],
     defaultRules: [(v) => !!v || "E necessario preencher o campo"],
     emailRules: [
       (v) => !!v || "E necessario preencher o campo",
@@ -120,6 +153,9 @@ export default {
     cleanFields() {
       this.fields = {};
     },
+    push() {
+       this.panel = 1
+    }
   },
 };
 </script>
